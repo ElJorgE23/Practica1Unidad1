e4e08c8c844094edc1bb4d612e1e142d8ed0a4fc
ElJorgE23 <s21120186@alumnos.itsur.edu.mx>
2025-02-15 21:27:21 -0600

    Ahora se crearon los Componentes de Dagger

--- basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/UserRepository.kt
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/src/main/java/com/example/marsphotos/network/UserRepository.kt
@@ -1,5 +1,7 @@
 package com.example.marsphotos.network
 
+import com.google.android.datatransport.runtime.dagger.Component
+
 class UserRepository @Inject constructor(
     private val localDataSource: UserLocalDataSource,
     private val remoteDataSource: UserRemoteDataSource
@@ -7,3 +9,10 @@ class UserRepository @Inject constructor(
 // @Inject lets Dagger know how to create instances of these objects
 class UserLocalDataSource @Inject constructor() { }
 class UserRemoteDataSource @Inject constructor() { }
+// @Component makes Dagger create a graph of dependencies
+@Component
+interface ApplicationGraph {
+    // The return type  of functions inside the component interface is
+    // what can be provided from the container
+    fun repository(): UserRepository
+}
